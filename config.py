78c42e26a5cef1355ba6982249c0b9274dd01867
ddedd5 <Pallabisanyal77@gmail.com>
2025-07-11 13:21:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002736281938"))
 
 
 # Mongo DB Database Config
-DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://sohamdebnathwg:Ai8RyKuaak7awEuN@cluster0.wks1ea5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URL = os.environ.get("DATABASE_URL", mongodb+srv://sohamdebnathwg:Ai8RyKuaak7awEuN@cluster0.wks1ea5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "thunder")
 
 
